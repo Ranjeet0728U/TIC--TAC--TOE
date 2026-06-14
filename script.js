972e13673a9a51dcd0ce0bd61bce77381217706f
Ranjeet1228 <upadhyayr0728@gamil.com>
2026-06-15 02:00:46 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,76 @@
-const first = document.getElementById();
-const second = document.getElementById();
-const third = document.getElementById();
-const fourth = document.getElementById();
-const fifth = document.getElementById();
-const sixth = document.getElementById();
-const seventh = document.getElementById();
-const eighth = document.getElementById();
-const nineth = document.getElementById();
+const first = document.getElementById('01');
+const second = document.getElementById('02');
+const third = document.getElementById('03');
+const fourth = document.getElementById('04');
+const fifth = document.getElementById('05');
+const sixth = document.getElementById('06');
+const seventh = document.getElementById('07');
+const eighth = document.getElementById('08');
+const nineth = document.getElementById('09');
+
+first.addEventListener('click',() =>{
+    addText(first)
+})
+
+
+second.addEventListener('click',() =>{
+    addText(second);
+})
+
+
+third.addEventListener('click',() =>{
+    addText(third);
+
+})
+
+
+fourth.addEventListener('click',() =>{
+    addText(fourth);
+})
+
+
+fifth.addEventListener('click',() =>{
+    addText(fifth);
+})
+
+
+sixth.addEventListener('click',() =>{
+    addText(sixth);
+})
+
+
+seventh.addEventListener('click',() =>{
+    addText(seventh);
+})
+
+
+eighth.addEventListener('click',() =>{
+    addText(eighth);
+})
+
+
+nineth.addEventListener('click',() =>{
+    addText(nineth);
+})
 
 let count = 0;
+
+function addText(index){
+    if(index.textContent !== ''){
+        alert("already filled");
+        return;
+    }
+    let ele = null;
+    if(count  % 2 == 0){
+        ele = 'X'
+    }else{
+        ele = 'O'
+    }
+
+    const span = document.createElement('span');
+    span
+    span.textContent = ele;
+    
+    index.appendChild(span);
+    count++;
+}
