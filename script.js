81c944bb8c6c86f1f1c3a1c51a4bcc28782efb29
Ranjeet1228 <upadhyayr0728@gamil.com>
2026-06-15 01:38:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const first = document.getElementById();
+const second = document.getElementById();
+const third = document.getElementById();
+const fourth = document.getElementById();
+const fifth = document.getElementById();
+const sixth = document.getElementById();
+const seventh = document.getElementById();
+const eighth = document.getElementById();
+const nineth = document.getElementById();
+
+let count = 0;
