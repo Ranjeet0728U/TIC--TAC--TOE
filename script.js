dc7ea97777e02fb0b1fd94d4e014cbce1b0c715e
Ranjeet1228 <upadhyayr0728@gamil.com>
2026-06-15 09:36:43 +0530

    final

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ document.querySelectorAll('.inner-container').forEach((ele) =>{
     ele.addEventListener('click', () =>{
         const element = addText(ele);
         addToArray(ele.id,element);
+        if(checckWinner()){
+            alert(`${element} is winner 🎉`);
+            location.reload();
+        }
+        if(!isSpaceLeft()){
+            alert("No one is winner");
+            location.reload();
+        }
     })
 })
 
@@ -41,4 +49,50 @@ function addToArray(id,ele){
     if(array[row][col] == null){
         array[row][col] = ele;
     }
+}
+function checckWinner(){
+    const check1 = array[0][0] === array[1][0] 
+            && array[1][0]=== array[2][0];
+
+    const check2 = array[0][1] === array[1][1]
+            && array[1][1]=== array[2][1];
+
+    const check3 = array[0][2] === array[1][2] 
+            && array[1][2]=== array[2][2];
+
+    const check4 = array[0][0] === array[0][1] 
+            && array[0][1]=== array[0][2];
+
+    const check5 = array[1][0] === array[1][1] 
+            && array[1][1]=== array[1][2];
+
+    const check6 = array[2][0] === array[2][1] 
+            && array[2][1]=== array[2][2];
+
+
+    const check7 = array[0][0] === array[1][1] 
+            && array[1][1]=== array[2][2];
+
+    const check8 = array[0][2] === array[1][1] 
+            && array[1][1]=== array[2][0];
+
+    if(check1 && array[0][0] != null) return true;
+    if(check2 && array[0][1] != null) return true;
+    if(check3 && array[0][2] != null) return true;
+    if(check4 && array[0][0] != null) return true;
+    if(check5 && array[1][0] != null) return true;
+    if(check6 && array[2][0] != null) return true;
+    if(check7 && array[0][0] != null) return true;
+    if(check8 && array[1][1] != null) return true;
+    return false;
+}
+
+function isSpaceLeft(){
+    for(let  i = 0; i < array.length; i++){
+        for(let j = 0; j < array[0].length; j++){
+            if(array[i][j] == null) return true;
+        }
+    }
+
+    return false;
 }
